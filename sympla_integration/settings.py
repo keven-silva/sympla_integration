15a8f898d7ef25a793baa9afc343441f478614c3
keven silva <keven.almeida.8179@gmail.com>
2025-07-22 19:36:25 -0300

    Updated settings

--- sympla_integration/settings.py
+++ sympla_integration/settings.py
@@ -77,10 +77,6 @@ WSGI_APPLICATION = 'sympla_integration.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('POSTGRES_DB'),
